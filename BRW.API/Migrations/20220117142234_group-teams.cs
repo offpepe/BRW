c7efe97cd6ac2b9a1a1817f29637822674e739b7
Alan Albuquerque Ferreira Lopes <alan.lopes@meta.com.br>
2022-01-17 11:51:08 -0300

    feat: first migration

---
+++ BRW.API/Migrations/20220117142234_group-teams.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BRW.API.Migrations
+{
+    public partial class groupteams : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_GroupId",
+                table: "Teams",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teams_Groups_GroupId",
+                table: "Teams",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teams_Groups_GroupId",
+                table: "Teams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teams_GroupId",
+                table: "Teams");
+        }
+    }
+}
