4f1da06c734f2af055cc07b428c1fbad8ef5317d
Alan Albuquerque Ferreira Lopes <alan.lopes@meta.com.br>
2022-01-17 11:50:36 -0300

    feat: dotnet new web-api

---
+++ BRW.API/Context/BrwAppContext.cs
@@ -0,0 +1,22 @@
+using BRW.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BRW.API.Context;
+
+public class BrwAppContext: DbContext
+{
+    public BrwAppContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    
+    public DbSet<Team> Teams { get; set; }
+
+    public DbSet<Group> Groups { get; set; }
+    
+    public DbSet<Game> Games { get; set; }
+    
+    public DbSet<Prize> Prizes { get; set; }
+
+}
---
+++ BRW.API/Program.cs
@@ -0,0 +1,33 @@
+using BRW.API.Context;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("postgresConnectionString");
+builder.Services.AddDbContext<BrwAppContext>(options => options.UseNpgsql(connectionString));
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
