3986ec861360d82385183970829101386cd3819c
Alan Albuquerque Ferreira Lopes <alan.lopes@meta.com.br>
2022-01-19 01:38:25 -0300

    perf: bindNever dataAnnotation on id property

--- BRW.API/Entities/Basis.cs
+++ BRW.API/Entities/Basis.cs
@@ -2,14 +2,18 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BRW.API.Entities;
 
 public abstract class Basis
 {
     [Key]
+    [BindNever]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [BindNever]
     public Guid Pid { get; set; }
     [Required]
     [StringLength(100)]
