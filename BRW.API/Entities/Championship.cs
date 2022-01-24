dec19d6d4889245bbc3963c382cafed8c75b4aed
Alan Albuquerque Ferreira Lopes <alan.lopes@meta.com.br>
2022-01-24 12:42:40 -0300

    BREAKING CHANGE: changed project arch to DDD
    the project was initial in MVC arch, but in was found out that Data Driven Domain does everything that we need and is scalable to future feature, like microsservices in ``golang`` and ``php``

--- BRW.API/Entities/Championship.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace BRW.API.Entities;
-
-public class Championship : Basis
-{
-
-}
-
--- BRW.API/Entities/Prize.cs
+++
@@ -1,7 +0,0 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace BRW.API.Entities;
-
-public class Prize: Basis
-{
-}
