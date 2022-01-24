2ee9d36d00be8b7034ef926ae2efcb5ed2d1c48b
Alan Albuquerque Ferreira Lopes <alan.lopes@meta.com.br>
2022-01-24 15:01:41 -0300

    feat: database config

--- BRW.WebApi/Program.cs
+++ BRW.WebApi/Program.cs
@@ -1,9 +1,15 @@
+using BRW.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var connectionString = builder.Configuration.GetConnectionString("postgresConnectionString");
+builder.Services.AddDbContext<BrwContext>(options => options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
